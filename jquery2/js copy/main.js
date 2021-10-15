26ef8fd13c9c00643887e1859b0aac54d67a904e
jisun22 <hayanangela@gmail.com>
2021-10-15 16:33:59 +0900

    커밋

--- jquery2/js copy/main.js
+++
@@ -1,8 +0,0 @@
-const ToggleBtn = document.querySelector('.header_ToggleBtn');
-const menu = document.querySelector('.header_menu');
-const icons = document.querySelector('.header_icons');
-
-ToggleBtn.addEventListener('click',() =>{
-    menu.classList.ToggleBtn('active');
-    icons.classList.ToggleBtn('active');
-});
